1062cdb22a347b130d7541a1c87932f5b4019838
iamshaunjp <shaun@thenetninja.co.uk>
2025-02-21 15:51:15 +0000

    lesson-1

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const index = () => {
+  return (
+    <View>
+      <Text>Shelfie App</Text>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
