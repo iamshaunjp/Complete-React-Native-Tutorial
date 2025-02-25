b3cf84e4fa0c9a24f1d0239efbac05b44439883e
iamshaunjp <shaun@thenetninja.co.uk>
2025-02-25 14:56:26 +0000

    lesson-7

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -14,6 +14,9 @@ export default function RootLayout() {
         headerStyle: { backgroundColor: theme.navBackground },
         headerTintColor: theme.title,
       }}>
+        {/* Groups */}
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+
         {/* Individual Screens */}
         <Stack.Screen name="index" options={{ title: "Home" }} />
         <Stack.Screen name="about" options={{ title: "About" }} />
