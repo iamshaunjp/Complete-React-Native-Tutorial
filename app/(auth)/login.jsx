ef095c084700797581593fbcc2ddf530e4f90bf6
iamshaunjp <shaun@thenetninja.co.uk>
2025-03-01 11:09:28 +0000

    lesson-8

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,11 +1,17 @@
-import { StyleSheet } from 'react-native'
+import { Pressable, StyleSheet, Text } from 'react-native'
 import { Link } from 'expo-router'
+import { Colors } from '../../constants/Colors'
 
 import ThemedView from '../../components/ThemedView'
 import ThemedText from '../../components/ThemedText'
 import Spacer from '../../components/Spacer'
 
 const Login = () => {
+
+  const handleSubmit = async () => {
+    console.log('login form submitted')
+  }
+
   return (
     <ThemedView style={styles.container}>
       
@@ -13,6 +19,13 @@ const Login = () => {
       <ThemedText title={true} style={styles.title}>
         Login to Your Account
       </ThemedText>
+
+      <Pressable
+        onPress={handleSubmit}
+        style={({ pressed }) => [styles.btn, pressed && styles.pressed]}
+      >
+        <Text style={{color: "#f2f2f2"}}>Login</Text>
+      </Pressable>
 
       <Spacer height={100} />
       <Link href="/register" replace>
@@ -31,10 +44,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+    alignItems: "center"
   },
   title: {
     textAlign: "center",
     fontSize: 18,
     marginBottom: 30
   },
+  btn: {
+    backgroundColor: Colors.primary,
+    padding: 15,
+    borderRadius: 5,
+  },
+  pressed: {
+    opacity: 0.8
+  }
 })
