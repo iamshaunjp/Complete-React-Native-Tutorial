d7b2c6dd9b8445a28936202a86daaff81b522b57
Shaun (Net Ninja) <9838872+iamshaunjp@users.noreply.github.com>
2025-03-01 11:10:52 +0000

    Update _layout.jsx

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -19,8 +19,6 @@ export default function RootLayout() {
 
         {/* Individual Screens */}
         <Stack.Screen name="index" options={{ title: "Home" }} />
-        <Stack.Screen name="about" options={{ title: "About" }} />
-        <Stack.Screen name="contact" options={{ title: "Contact", headerShown: false }} />
       </Stack>
     </>
   )
