389e85b9eddbbc44cdf3a1493df01d0e99285d69
iamshaunjp <shaun@thenetninja.co.uk>
2025-03-09 14:24:30 +0000

    lesson-19

--- app/(dashboard)/_layout.jsx
+++ app/(dashboard)/_layout.jsx
@@ -3,11 +3,14 @@ import { useColorScheme } from "react-native"
 import { Colors } from "../../constants/Colors"
 import { Ionicons } from "@expo/vector-icons"
 
+import UserOnly from "../../components/auth/UserOnly"
+
 export default function DashboardLayout() {
   const colorScheme = useColorScheme()
   const theme = Colors[colorScheme] ?? Colors.light
 
   return (
+    <UserOnly>
     <Tabs
       screenOptions={{
         headerShown: false,
@@ -47,5 +50,6 @@ export default function DashboardLayout() {
         )}} 
       />
     </Tabs>
+    </UserOnly>
   )
 }
